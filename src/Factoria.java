6ac72a2e74e97657f6e99b5265f2c138dbad6737
Borja G.B <bgarciabarrera@danielcastelao.org>
2023-03-10 09:19:47 +0100

    second commit

---
+++ src/Factoria.java
@@ -0,0 +1,44 @@
+import com.jay.transportes.Camion;
+import com.jay.transportes.Bicicleta;
+import com.jay.transportes.ITransportes;
+
+import javax.swing.*;
+
+/**
+ * Fabrica encargada de devolver el producto
+ * que elegimos según el tipo
+ */
+public class Factoria {
+    public static final int CAMION = 1;
+    public static final int BICICLETA = 2;
+    public static Integer cp = null;
+    public static Float dimensionX = null;
+    public static Float dimensionY = null;
+    public static Float dimensionZ = null;
+    public static Float peso = null;
+    /**
+     * Devuelve producto del tipo de la Interface
+     * @param type 1: consola, 2:ventana
+     * @param msg string a imprimir
+     * @return producto segun el tipo
+     */
+    public static ITransportes getProducto(int type) {
+        cp = Integer.parseInt(JOptionPane.showInputDialog("Teclee tu CP"));
+        dimensionX = Float.parseFloat(JOptionPane.showInputDialog("Teclee a dimensionX"));
+        dimensionY = Float.parseFloat(JOptionPane.showInputDialog("Teclee a dimensionY"));
+        dimensionZ = Float.parseFloat(JOptionPane.showInputDialog("Teclee a dimensionZ"));
+        peso = Float.parseFloat(JOptionPane.showInputDialog("Teclee o peso"));
+        switch (type) {
+            // tipo consola
+            case CAMION:
+                return new Camion(cp, dimensionX, dimensionY, dimensionZ, peso);
+            // tipo ventana
+            case BICICLETA:
+                return new Bicicleta(cp, dimensionX, dimensionY, dimensionZ, peso);
+            // otro tipo
+            default:
+                return null;
+        }
+    }
+
+}
